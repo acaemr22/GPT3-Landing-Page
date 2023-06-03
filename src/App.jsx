727f34d69f309b6b9a686843ec7d32097b2f904c
Emre Açar <ea517645@gmail.com>
2023-06-04 00:55:32 +0300

    Initial Commit.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Blog,
+  Footer,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./Components/index";
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className="gradient__bg">
+            <Navbar/>
+            <Header />
+        </div>
+
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+
+    </div>
+  );
+};
+
+export default App;
